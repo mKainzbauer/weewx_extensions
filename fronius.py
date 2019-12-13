d0c43d0380d9a62e4421a7e6fddd76925e42e1eb
mKainzbauer <58649043+mKainzbauer@users.noreply.github.com>
2019-12-13 17:56:06 +0100

    Fixes, logging

--- fronius.py
+++ fronius.py
@@ -1,3 +1,37 @@
+# -*- coding: utf-8 -*-
+#    See the file LICENSE.txt for your rights.
+#    Author: Michael Kainzbauer (github: mkainzbauer)
+
+"""How to read data from your Fronius device and store it as radiation in weewx archive data
+
+*******************************************************************************
+
+To use this extension, add the following somewhere in your configuration file
+weewx.conf:
+
+[Fronius]
+    api_url = http://IP_ADRESS.OF_YOUR.DEVICE.HERE/solar_api/v1/GetArchiveData.cgi?
+    timeZone = Z # use the timeZone of your device 
+    installedWP = 3000 # installed Watts peak
+    
+
+*******************************************************************************
+
+To enable this service:
+
+1) copy this file to the user directory
+
+2) modify the weewx configuration file by adding this service to the option
+"report_services", located in section [Engine][[Services]].
+
+[Engine]
+  [[Services]]
+    ...
+    data_services = user.fronius.AddRadiation # other services can be appended separated with a comma
+
+*******************************************************************************
+"""
+
 import weewx, json, urllib2, syslog, weeutil.weeutil
 from weeutil.weeutil import timestamp_to_string
 from weewx.engine import StdService
@@ -15,14 +49,16 @@ class AddRadiation(StdService):
 
     def new_archive_record(self, event):
       radiation = self.getRadiation(event)
-      syslog.syslog(syslog.LOG_INFO, "Radiation: %s at %s" % (radiation, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
+      syslog.syslog(syslog.LOG_DEBUG, "Radiation: %s at %s" % (radiation, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
       event.record['radiation'] = radiation
 
     def getRadiation(self, event):
       timeZone = "Z"
       if 'timeZone' in self.config_dict['Fronius']:
         timeZone = self.config_dict['Fronius']['timeZone']
-        syslog.syslog(syslog.LOG_INFO, "Fronius timeZone: %s"  % timeZone)
+        syslog.syslog(syslog.LOG_DEBUG, "Fronius timeZone: %s"  % timeZone)
+      installedWP = float(self.config_dict['Fronius']['installedWP'])
+      syslog.syslog(syslog.LOG_DEBUG, "Installed Watt peak: %s"  % installedWP)
       startDateTime = int(event.record['dateTime'])
       endDateTime = startDateTime + int(self.config_dict['StdArchive']['archive_interval'])
       endDate = datetime.utcfromtimestamp(endDateTime).isoformat() + timeZone
@@ -35,7 +71,10 @@ class AddRadiation(StdService):
         work = data["Body"]["Data"]["inverter/1"]["Data"]["EnergyReal_WAC_Sum_Produced"]["Values"]["0"]
         timeSpan = data["Body"]["Data"]["inverter/1"]["Data"]["TimeSpanInSec"]["Values"]["0"]
         averagePower = work / (timeSpan / float(3600))
-        return round(averagePower)
+        syslog.syslog(syslog.LOG_INFO, "Avg Power: %s at %s" % (averagePower, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
+        normalizedOutput = averagePower / installedWP
+        syslog.syslog(syslog.LOG_DEBUG, "Normalized Output: %s at %s" % (normalizedOutput, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
+        return normalizedOutput
       except urllib2.URLError, e:
         syslog.syslog(syslog.LOG_ERR, "Error getting inverter data: %r" % e)
       except:
