5814cb3a8e56895f90ca6c768c5fd45aa4edc712
mKainzbauer <58649043+mKainzbauer@users.noreply.github.com>
2020-05-18 09:22:50 +0200

    Update fronius.py
    Changes to work with python3 and weewx 4.x

--- fronius.py
+++ fronius.py
@@ -2,7 +2,7 @@
 #    See the file LICENSE.txt for your rights.
 #    Author: Michael Kainzbauer (github: mkainzbauer)
 
-"""How to read data from your Fronius device and store it as radiation in weewx archive data
+"""How to read data from your Fronius device and store it as as selected value in weewx archive data
 
 *******************************************************************************
 
@@ -13,6 +13,7 @@ weewx.conf:
     api_url = http://IP_ADRESS.OF_YOUR.DEVICE.HERE/solar_api/v1/GetArchiveData.cgi?
     timeZone = Z # use the timeZone of your device 
     installedWP = 3000 # installed Watts peak
+    archive_column = signal1 #optional, "radiation" is default
     
 
 *******************************************************************************
@@ -27,55 +28,62 @@ To enable this service:
 [Engine]
   [[Services]]
     ...
-    data_services = user.fronius.AddRadiation # other services can be appended separated with a comma
+    data_services = user.fronius.AddElectricity # other services can be appended separated with a comma
 
 *******************************************************************************
 """
 
-import weewx, json, urllib2, syslog, weeutil.weeutil
+import weewx, json, urllib.request, urllib.error, weeutil.weeutil, logging
 from weeutil.weeutil import timestamp_to_string
 from weewx.engine import StdService
 from datetime import datetime
 
-class AddRadiation(StdService):
+log = logging.getLogger(__name__)
+
+class AddElectricity(StdService):
       
     def __init__(self, engine, config_dict):
 
       # Initialize my superclass first:
-      super(AddRadiation, self).__init__(engine, config_dict)
+      super(AddElectricity, self).__init__(engine, config_dict)
 
       # Bind to any new archive record events:
       self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
 
     def new_archive_record(self, event):
-      radiation = self.getRadiation(event)
-      syslog.syslog(syslog.LOG_DEBUG, "Radiation: %s at %s" % (radiation, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
-      event.record['radiation'] = radiation
+      value = self.getValue(event)
+      log.debug("Inverter Value: %s at %s" % (value, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
+      
+      archive_column = "radiation"
+      if 'archive_column' in self.config_dict['Fronius']:
+        archive_column = self.config_dict['Fronius']['archive_column']
+      
+      event.record[archive_column] = value
 
-    def getRadiation(self, event):
+    def getValue(self, event):
       timeZone = "Z"
       if 'timeZone' in self.config_dict['Fronius']:
         timeZone = self.config_dict['Fronius']['timeZone']
-        syslog.syslog(syslog.LOG_DEBUG, "Fronius timeZone: %s"  % timeZone)
+        log.debug("Fronius timeZone: %s"  % timeZone)
       installedWP = float(self.config_dict['Fronius']['installedWP'])
-      syslog.syslog(syslog.LOG_DEBUG, "Installed Watt peak: %s"  % installedWP)
+      log.debug("Installed Watt peak: %s"  % installedWP)
       startDateTime = int(event.record['dateTime'])
       endDateTime = startDateTime + int(self.config_dict['StdArchive']['archive_interval'])
       endDate = datetime.utcfromtimestamp(endDateTime).isoformat() + timeZone
       startDate = datetime.utcfromtimestamp(startDateTime).isoformat() + timeZone
       url = self.config_dict['Fronius']['api_url'] + 'Scope=System&StartDate=' + startDate + '&EndDate=' + endDate + '&Channel=EnergyReal_WAC_Sum_Produced&Channel=TimeSpanInSec'
-      syslog.syslog(syslog.LOG_INFO, url)
+      log.info(url)
       try:
-        response = urllib2.urlopen(url)
+        response = urllib.request.urlopen(url)
         data = json.loads(response.read())
         work = data["Body"]["Data"]["inverter/1"]["Data"]["EnergyReal_WAC_Sum_Produced"]["Values"]["0"]
         timeSpan = data["Body"]["Data"]["inverter/1"]["Data"]["TimeSpanInSec"]["Values"]["0"]
         averagePower = work / (timeSpan / float(3600))
-        syslog.syslog(syslog.LOG_INFO, "Avg Power: %s at %s" % (averagePower, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
+        log.info("Avg Power: %s at %s" % (averagePower, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
         normalizedOutput = averagePower / installedWP
-        syslog.syslog(syslog.LOG_DEBUG, "Normalized Output: %s at %s" % (normalizedOutput, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
+        log.debug("Normalized Output: %s at %s" % (normalizedOutput, (weeutil.weeutil.timestamp_to_string(event.record['dateTime']))))
         return normalizedOutput
-      except urllib2.URLError, e:
-        syslog.syslog(syslog.LOG_ERR, "Error getting inverter data: %r" % e)
+      except urllib.error.URLError as e:
+        log.error("Error getting inverter data: %r" % e)
       except:
-        syslog.syslog(syslog.LOG_ERR, "Unexpected error:")
+        log.error("Unexpected error:")
